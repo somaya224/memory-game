263b40827238125548ae0cf57fe70522421aec18
somaya224 <somaya224@yahoo.com>
2019-01-03 21:05:21 -0500

    Add responsive.css and READMe.md files

--- js/app.js
+++ js/app.js
@@ -19,6 +19,9 @@ const fAwsmClasses = ['fa-anchor',
 // create the div deck contains the cards
 let deck = document.createElement('div');
 deck.classList.add('deck');
+let deckFragment = document.createDocumentFragment(); // for deck
+let cardContainerFragement = document.createDocumentFragment(); // for cardContainer
+
 fAwsmClasses.forEach(function(elem) {
   let cardContainer = document.createElement('div');
   let cardFront = document.createElement('div');
@@ -30,31 +33,17 @@ fAwsmClasses.forEach(function(elem) {
   cardFront.classList.add('card-front');
   i.classList.add('fa', elem);
 
-  cardContainer.appendChild(cardBack);
+  cardContainerFragement.appendChild(cardBack);
   cardFront.appendChild(i);
-  cardContainer.appendChild(cardFront);
-  deck.appendChild(cardContainer);
+  cardContainerFragement.appendChild(cardFront);
+  cardContainer.appendChild(cardContainerFragement);
+  deckFragment.appendChild(cardContainer);
 });
 
-let divContainer = document.querySelector('.container');
-divContainer.appendChild(deck);
+let cardsList = deckFragment.querySelectorAll('.card-container');
+let cardsArray = Array.from(cardsList);
 
-let cardsNodeList = deck.querySelectorAll('.card-container');
-// this return a nodelist which is immutable so we cant shuffle it
-// we need to convert it to an array using Array.from()
-let cardsArray = Array.from(cardsNodeList);
-
-
-/* Shuffle  li's to arrange cards randomly on the grid
-======================================================
-*/
-
-/* Notes for me to remember:
-    1.  This is called Fisher -Yates Modern Shuffle Algorithm.
-    2. The loop should iterate over array from back to front bassing index 0.
-    3. Each loop pass generate a random number ranging between 0 and array length.
-    4. Each loop pass swap the current index value in the loop with (the last element the loop iterate)
-with index the random  index  (number) generated each loop.*/
+// Shuffle  li's to arrange cards randomly on the grid
 function shuffle(cardsArray) {
   var currentIndex = cardsArray.length - 1, randomIndex, temporaryValue;
   for (currentIndex; currentIndex > 0; currentIndex--) {
@@ -66,9 +55,23 @@ function shuffle(cardsArray) {
 }
 
 // suffle the card with the browser refresh
-shuffle(cardsArray)
+shuffle(cardsArray);
+
+// clear decFragment
+deckFragment.querySelectorAll('.card-container').forEach(c => deckFragment.removeChild(c));
+
+// add the new shuffled cards into the deckFragment
+cardsArray.forEach(function(card) {
+  deckFragment.appendChild(card);
+});
+
 deck.innerHTML = '';
-cardsArray.forEach(cardContainer => deck.appendChild(cardContainer));
+deck.appendChild(deckFragment);
+// clear decFragment
+deckFragment.querySelectorAll('.card-container').forEach(c => deckFragment.removeChild(c));
+
+let container = document.querySelector('.container');
+container.appendChild(deck);
 
 // suffle the card with the div.restart button
 let restart = document.querySelector('.restart');
@@ -76,90 +79,130 @@ restart.onclick = function() {
   window.location.reload();
 };
 
+// set interval
+let timerStarted = false;
+let timer = false;
+let msec = 0, sec = 0, min = 0;
+let stopwatch = document.querySelector('.stopwatch');
+function run() {
+  stopwatch.innerText = (min < 10 ? '0' + min : min) + 'm :' +  (sec < 10 ? '0' + sec : sec) + 's :' + (msec < 10 ? '0' + msec : msec) + 'ms';
+  msec++;
+  if(msec === 100) {
+    msec = 0;
+    sec++
+  }
+  if (sec === 60) {
+    sec = 0;
+    min++;
+  }
+}
+
+//  addAventListener to deck and use Event Delegation and phases of event to reach each individual card
 let cards = document.querySelectorAll('.card-container');
   let count = 0;
   let twoCardsArr = [];
   let match = 0;
-// loop through all cards to addAventListener
-cards.forEach(function(card) {
-  // name of handler is click and event handler is Respond function
-  card.addEventListener('click', Respond);
-  function Respond(e) {
-    count++;
-    // show the card on click
-    let parent = e.target.parentElement;
-    let cardContent = parent.querySelector('.card-front');
-    cardContent.classList.add('card-front-open');
-    let cardWall = parent.querySelector('.card-back');
-    cardWall.classList.add('card-back-close');
-    let moves = document.querySelector('.moves');
-    twoCardsArr.push(parent);
+deck.addEventListener('click', respond, false);
+function respond(e) {
+  if (timerStarted === false) {
+    timer = setInterval(run, 10);
+    timerStarted = true;
+  }
 
-    // handleing the stars rate
-    let stars = document.querySelectorAll('.fa-star');
-    if(count === 20) {
-      stars[2].classList.remove('fa-star');
-      stars[2].classList.add('fa-star-o');
-    } else if(count === 30) {
-      stars[1].classList.remove('fa-star');
-      stars[1].classList.add('fa-star-o');
-    }
-    if(count % 2 === 0) {
-      // hangling moves number
-      moves.innerHTML = count / 2;
-      // Adding class match to  every two identical cards
-      // And remove the event handler from the two matched elements
-      if(twoCardsArr.length === 2) {
-        if(twoCardsArr[0].querySelector('i').classList.value === twoCardsArr[1].querySelector('i').classList.value){
-          twoCardsArr.forEach(function(card){
-            card.querySelector('.card-front-open').classList.add('match');
-            card.removeEventListener('click', Respond);
-          });
-          twoCardsArr = [];
-          match++;
-          if(match === 8) {
-            document.querySelector('.container').remove();
-            // document.body.container.remove();
-            let congrats = document.createElement('div');
-            divContainer.appendChild(congrats);
-            congrats.classList.add('congrats');
-            let congMessage = document.createElement('h1');
-            congMessage.classList.add('congMessage')
-            congMessage.innerText = 'Congratulations! You Won!';
-            congrats.appendChild(congMessage);
-            let congDetails = document.createElement('p');
-            congDetails.classList.add('details');
-            console.log(moves.innerText);
-            console.log(stars.length);
-            congrats.appendChild(congDetails);
-            // congDetails.innerText = "kjjhhjhfhj";
-            congDetails.innerText = `With ${moves.innerText} Moves and ${stars.length} Stars.`;
-            let cheer = document.createElement('p');
-            cheer.classList.add('details');
-            cheer.innerText = 'Wooooooo!';
-            congrats.appendChild(cheer);
-            let repeatBtn = document.createElement('div');
-            repeatBtn.classList.add('repeat');
-            repeatBtn.innerText = 'Play again!';
-            congrats.appendChild(repeatBtn);
-            document.body.appendChild(congrats);
-            repeatBtn.onclick = function() {
-              window.location.reload();
-            };
-          }
-        } else {
-          // Adding class not-match to the every two different cards
-          twoCardsArr.forEach(card => card.querySelector('.card-front-open').classList.add('not-match'));
-          // Close cards after 400ms by removing class card-front-open and also remove class not-match
-          window.setTimeout(function() {
-            twoCardsArr.forEach(card => card.querySelector('.card-front-open').classList.remove('card-front-open', 'not-match'));
-            twoCardsArr.forEach(card => card.querySelector('.card-back-close').classList.remove('card-back-close', 'not-match'));
-            // clear moves
-            twoCardsArr = [];
-          }, 400);
-        }
-      }
+  // Make sure that the target will be cards not the deck
+  let target = e.target;
+  if (target.classList.contains('deck')) return;
+  if (target.classList.contains('card-container')) return;
+
+  // show the card on click
+  let cardWall = target;
+  let cardContent = target.nextSibling;
+  if (!cardContent) return;
+  if (cardContent && cardContent.classList.contains('card-front-open')) return;
+  if(cardContent && cardContent.classList.contains('match')) return;
+  cardContent.classList.add('card-front-open');
+  cardWall.classList.add('card-back-close');
+  twoCardsArr.push(cardContent.parentElement);
+
+  // handleing the stars rate
+  count++;
+  let stars = document.querySelectorAll('.fa-star');
+  if(count === 20) {
+    stars[2].classList.remove('fa-star');
+    stars[2].classList.add('fa-star-o');
+  } else if(count === 30) {
+    stars[1].classList.remove('fa-star');
+    stars[1].classList.add('fa-star-o');
+  }
+
+  // hangling moves number
+  let moves = document.querySelector('.moves');
+  if(count % 2 === 0) {
+    moves.innerHTML = count / 2;
+  }
+
+  // Adding class match to  every two identical cards
+  // And remove the event handler from the two matched elements
+  if(twoCardsArr.length === 2) {
+    if(twoCardsArr[0].querySelector('i').classList.value === twoCardsArr[1].querySelector('i').classList.value){
+      twoCardsArr.forEach(card => card.querySelector('.card-front-open').classList.add('match'));
+      twoCardsArr = [];
+      match++;
+    } else {
+      // Adding class not-match to the every two different cards
+      twoCardsArr.forEach(card => card.querySelector('.card-front-open').classList.add('not-match'));
+      // Close cards after 400ms by removing class card-front-open and also remove class not-match
+      window.setTimeout(function() {
+        twoCardsArr.forEach(card => card.querySelector('.card-front-open').classList.remove('card-front-open', 'not-match'));
+        twoCardsArr.forEach(card => card.querySelector('.card-back-close').classList.remove('card-back-close', 'not-match'));
+        // clear moves
+        twoCardsArr = [];
+      }, 400);
     }
   }
-});
+  if(match === fAwsmClasses.length / 2) {
+    clearInterval(timer);
+
+    let loader = document.createElement('div');
+    let tick = document.createElement('div');
+    let congrats = document.createElement('div');
+    let congMessage = document.createElement('h1');
+    let congDetails = document.createElement('p');
+    let finalInterval = document.createElement('span');
+    let cheer = document.createElement('p');
+    let repeatBtn = document.createElement('div');
+
+    loader.classList.add('loader');
+    tick.classList.add('tick');
+    congrats.classList.add('congrats');
+    congMessage.classList.add('congMessage');
+    repeatBtn.classList.add('repeat');
+
+    tick.innerText = '✓';
+    congMessage.innerText = 'Congratulations! You Won!';
+    congDetails.innerText = `With ${moves.innerText} Moves and ${stars.length} Stars.`;
+    finalInterval.innerText = stopwatch.innerText;
+    cheer.innerText = 'Wooooooo!';
+    repeatBtn.innerText = 'Play again!';
+
+    loader.appendChild(tick);
+    window.setTimeout(function() {
+      document.querySelector('.container').remove();
+
+    deckFragment.appendChild(loader);
+    deckFragment.appendChild(congMessage);
+    deckFragment.appendChild(congDetails);
+    deckFragment.appendChild(finalInterval);
+    deckFragment.appendChild(cheer);
+    deckFragment.appendChild(repeatBtn);
+    congrats.appendChild(deckFragment);
+    }, 1000);
+
+    document.body.appendChild(congrats);
+    repeatBtn.onclick = function() {
+      window.location.reload();
+    };
+  }
+}
+
 
